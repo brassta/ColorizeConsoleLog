74b78b0ff269618603a5074aa473ccdc397c5b0d
brassta <brassta@gmail.com>
2021-01-23 17:20:53 +0100

    transfered logic to index.js

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-const unicornFun = (input, { postfix = 'rainbows' } = {}) => {
-  if(typeof input !== 'string'){
-    throw new TypeError(`Expected a string, got ${typeof input}`);
-  }
+export function ConsoleOrange(message, value1 = null, value2 = null, value3 = null) {
+  return [
+    `%c••• ${message}:%c`,
+    'background:#ff6600; color: white; font-weight:bold; padding:2px 10px 2px 4px;',
+    'background:transparent; color:transparent; width:0; overflow:hidden',
+    value1 ? value1 : '', value2 ? value2 : '', value3 ? value3 : ''
+  ];
+}
 
-  return `${input} & ${postfix}`;
-};
+export function ConsoleOrangeJS(message, value) {
+  return [
+    `%c••• ${message}:%c`,
+    'background:#ff6600; color: white; font-weight:bold; padding:2px 10px 2px 4px;',
+    'background:transparent; color:transparent; width:0; overflow:hidden',
+    JSON.stringify(value)
+  ];
+}
 
-export default unicornFun;
+export function ConsoleGray(message, value1 = null, value2 = null, value3 = null) {
+  return [
+    `%c••• ${message}:%c`,
+    'background: #888; color: white; font-weight:bold; padding:2px 15px 2px 4px; border-left:15px solid #00FF33;',
+    'background:transparent; color:transparent; width:0; overflow:hidden',
+    value1 ? value1 : '', value2 ? value2 : '', value3 ? value3 : ''
+  ];
+}
+
+export function ConsoleGrayJS(message, value) {
+  return [
+    `%c••• ${message}:%c`,
+    'background: #888; color: white; font-weight:bold; padding:2px 15px 2px 4px; border-left:15px solid #00FF33;',
+    'background:transparent; color:transparent; width:0; overflow:hidden',
+    JSON.stringify(value)
+  ];
+}
