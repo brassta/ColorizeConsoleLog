51508549a041754882adb334aacc65ca9c104b64
brassta <brassta@gmail.com>
2021-01-23 17:02:41 +0100

    first local commit

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 import test from 'ava';
 import unicornFun from './index.js';
+import {ConsoleGray, ConsoleGrayJS, ConsoleOrange, ConsoleOrangeJS} from './lib/colorize-console-log.js';
 
 test('main', t => {
 	t.throws(() => {
@@ -11,3 +12,25 @@ test('main', t => {
 
 	t.is(unicornFun('unicorns'), 'unicorns & rainbows');
 });
+
+test('cgray-basic', ((test)=>{
+  console.log(ConsoleGray('vidimo se console.log.gray'));
+  test.pass();
+}))
+
+test('cgray-stringify', ((test)=>{
+  console.log(ConsoleGrayJS('vidimo se console.log.gray json.stringify'));
+  test.pass();
+}))
+
+test('corange-basic', ((test)=>{
+  console.log(ConsoleOrange('vidimo se console.log.orange'));
+  test.pass();
+}))
+
+test('corange-stringify', ((test)=>{
+  console.log(ConsoleOrangeJS('vidimo se console.log.orange json.stringify'));
+  test.pass();
+}))
+
+console.log(ConsoleOrange('hoce li ovako da ga okine'));
