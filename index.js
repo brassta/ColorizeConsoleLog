c3a8a26707686d2e004acf304b74b65a20e9839d
BrankoStevanovic <brassta@gmail.com>
2021-08-27 15:40:11 +0200

    start adding mode for dark screen

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ export function ConsoleOrange(message, value1 = null, value2 = null, value3 = nu
   ];
 }
 
+export function ConsoleOrangeDS(message, value1 = null, value2 = null, value3 = null) {
+  return [
+    `%c••• ${message}:%c`,
+    'background:#ff6600; color: #888; font-weight:bold; padding:2px 10px 2px 4px;',
+    'background:transparent; color:transparent; width:0; overflow:hidden',
+    value1 ? value1 : '', value2 ? value2 : '', value3 ? value3 : ''
+  ];
+}
+
 export function ConsoleOrangeJS(message, value) {
   return [
     `%c••• ${message}:%c`,
