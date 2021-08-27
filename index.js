4cefadd984a78a2d823927a6ba45c2bcfe6d60e3
Branko Stevanovic <brassta@gmail.com>
2021-08-27 17:17:04 +0200

    green border again

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ export function ConsoleOrange(message, value1 = null, value2 = null, value3 = nu
 export function ConsoleOrangeDS(message, value1 = null, value2 = null, value3 = null) {
   return [
     `%c••• ${message}:%c`,
-    'background:#ff6600; color: #1E2952; font-weight:bold; padding:2px 10px 2px 4px;',
+    'background:#ff6600; color: #1E2952; font-weight:bold; padding:2px 10px 2px 4px; border-left:15px solid #00FF33;',
     'background:transparent; color:transparent; width:0; overflow:hidden',
     value1 ? value1 : '', value2 ? value2 : '', value3 ? value3 : ''
   ];
@@ -19,7 +19,7 @@ export function ConsoleOrangeDS(message, value1 = null, value2 = null, value3 = 
 export function ConsoleOrangeJS(message, value) {
   return [
     `%c••• ${message}:%c`,
-    'background:#ff6600; color: white; font-weight:bold; padding:2px 10px 2px 4px; border-left:15px solid #00FF33;',
+    'background:#ff6600; color: white; font-weight:bold; padding:2px 10px 2px 4px; ',
     'background:transparent; color:transparent; width:0; overflow:hidden',
     JSON.stringify(value)
   ];
