c7a83b07d29d1d1a7578d9f1a324d319a0e5c3c2
brassta <brassta@gmail.com>
2021-08-27 16:09:39 +0200

    dark mode v1

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ export function ConsoleOrange(message, value1 = null, value2 = null, value3 = nu
 export function ConsoleOrangeDS(message, value1 = null, value2 = null, value3 = null) {
   return [
     `%c••• ${message}:%c`,
-    'background:#ff6600; color: #888; font-weight:bold; padding:2px 10px 2px 4px;',
+    'background:#ff6600; color: #1E2952; font-weight:bold; padding:2px 10px 2px 4px;',
     'background:transparent; color:transparent; width:0; overflow:hidden',
     value1 ? value1 : '', value2 ? value2 : '', value3 ? value3 : ''
   ];
@@ -25,6 +25,15 @@ export function ConsoleOrangeJS(message, value) {
   ];
 }
 
+export function ConsoleOrangeJSDS(message, value) {
+  return [
+    `%c••• ${message}:%c`,
+    'background:#ff6600; color: #1E2952; font-weight:bold; padding:2px 10px 2px 4px;',
+    'background:transparent; color:transparent; width:0; overflow:hidden',
+    JSON.stringify(value)
+  ];
+}
+
 export function ConsoleGray(message, value1 = null, value2 = null, value3 = null) {
   return [
     `%c••• ${message}:%c`,
@@ -34,10 +43,28 @@ export function ConsoleGray(message, value1 = null, value2 = null, value3 = null
   ];
 }
 
+export function ConsoleGrayDS(message, value1 = null, value2 = null, value3 = null) {
+  return [
+    `%c••• ${message}:%c`,
+    'background: #454545; color: #DBDBDB; font-weight:bold; padding:2px 15px 2px 4px; border-left:15px solid #00FF33;',
+    'background:transparent; color:transparent; width:0; overflow:hidden',
+    value1 ? value1 : '', value2 ? value2 : '', value3 ? value3 : ''
+  ];
+}
+
 export function ConsoleGrayJS(message, value) {
   return [
     `%c••• ${message}:%c`,
     'background: #888; color: white; font-weight:bold; padding:2px 15px 2px 4px; border-left:15px solid #00FF33;',
+    'background:transparent; color:transparent; width:0; overflow:hidden',
+    JSON.stringify(value)
+  ];
+}
+
+export function ConsoleGrayJSDS(message, value) {
+  return [
+    `%c••• ${message}:%c`,
+    'background: #454545; color: #DBDBDB; font-weight:bold; padding:2px 15px 2px 4px; border-left:15px solid #00FF33;',
     'background:transparent; color:transparent; width:0; overflow:hidden',
     JSON.stringify(value)
   ];
